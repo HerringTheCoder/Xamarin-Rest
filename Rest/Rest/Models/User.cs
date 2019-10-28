3a40a072bfa12dfe3dbc56bd932d58a8b5ea98ac
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2019-10-28 11:11:26 +0100

    Add project files.

---
+++ Rest/Rest/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Rest.Models
+{
+    public class User
+    {
+        [JsonProperty("email")]
+        public string Email { set; get; }
+        [JsonProperty("password")]
+        public string Password { set; get; }
+    }
+}
---
+++ Rest/Rest/ViewModels/AboutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace Rest.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        [Obsolete]
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
